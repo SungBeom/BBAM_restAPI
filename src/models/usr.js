162ad3ab3150ef711907495bbba5b3a1847e1393
SungBeom <psb_94@naver.com>
2018-10-08 15:29:00 +0900

    DB connection_USR

--- src/models/usr.js
+++ src/models/usr.js
@@ -1,34 +1,34 @@
 module.exports = (sequelize, DataType) => sequelize.define('USR', {
   USR_ID: {
-    type: Sequelize.STRING(10),
+    type: DataType.STRING(10),
     primaryKey: true,
     allowNull: false
   },
   USR_PW: {
-    type: Sequelize.STRING(20),
+    type: DataType.STRING(20),
     allowNull: false
   },
   USR_NM: {
-    type: Sequelize.STRING(20),
+    type: DataType.STRING(20),
     allowNull: false
   },
   USR_ADDR_ST: {
-    type: Sequelize.STRING(10)
+    type: DataType.STRING(10)
   },
   USR_ADDR_CT: {
-    type: Sequelize.STRING(20)
+    type: DataType.STRING(20)
   },
   USR_ADDR_STRT: {
-    type: Sequelize.STRING(20)
+    type: DataType.STRING(20)
   },
   USR_SCHL: {
-    type: Sequelize.STRING(20)
+    type: DataType.STRING(20)
   },
   USR_GRD: {
-    type: Sequelize.TINYINT(1).UNSIGNED
+    type: DataType.TINYINT(1).UNSIGNED
   },
   USR_JNDT: {
-    type: Sequelize.DATE(3),
+    type: DataType.DATE(3),
     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)')
   }
 });
