c7bc1a56671c85985a225ed507e8b2b6e1aa9e05
SungBeom <psb_94@naver.com>
2018-10-08 16:11:20 +0900

    DB connection_PRB

--- src/models/prb.js
+++ src/models/prb.js
@@ -1,29 +1,29 @@
-module.exports = (sequelize, DataType) => sequelize.define('PRB', {
-  PRB_ID: {
-    type: DataType.INTEGER.UNSIGNED,
-    primaryKey: true,
-    allowNull: false,
-    autoIncrement: true
-  },
-  PRB_DIFF: {
-    type: DataType.TINYINT(1).UNSIGNED
-  },
-  PRB_CLS: {
-    type: DataType.STRING(10)
-  },
-  PRB_CNT: {
-    type: DataType.STRING(500)
-  },
-  PRB_HNT: {
-    type: DataType.STRING(500)
-  },
-  PRB_IN: {
-    type: DataType.STRING(100)
-  },
-  PRB_OUT: {
-    type: DataType.STRING(100)
-  },
-  PRB_RTN: {
-    type: DataType.INTEGER
-  }
+export default (sequelize, DataType) => sequelize.define('PRB', {
+    PRB_ID: {
+        type: DataType.INTEGER.UNSIGNED,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+    },
+    PRB_DIFF: {
+        type: DataType.TINYINT(1).UNSIGNED
+    },
+    PRB_CLS: {
+        type: DataType.STRING(10)
+    },
+    PRB_CNT: {
+        type: DataType.STRING(500)
+    },
+    PRB_HNT: {
+        type: DataType.STRING(500)
+    },
+    PRB_IN: {
+        type: DataType.STRING(100)
+    },
+    PRB_OUT: {
+        type: DataType.STRING(100)
+    },
+    PRB_RTN: {
+        type: DataType.INTEGER
+    }
 });
