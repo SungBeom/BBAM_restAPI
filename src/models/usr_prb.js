017b42db21bcac1397ece9a25c95e7eaaa031b87
SungBeom <psb_94@naver.com>
2018-10-08 16:12:58 +0900

    DB connection_USR_PRB

--- src/models/usr_prb.js
+++ src/models/usr_prb.js
@@ -1,24 +1,24 @@
-module.exports = (sequelize, DataType) => sequelize.define('USR_PRB', {
+export default (sequelize, DataType) => sequelize.define('USR_PRB', {
     UP_UID: {
-        type: Sequelize.STRING(10),
+        type: DataType.STRING(10),
         primaryKey: true,
         allowNull: false
     },
     UP_PID: {
-        type: Sequelize.INTEGER.UNSIGNED,
+        type: DataType.INTEGER.UNSIGNED,
         primaryKey: true,
         allowNull: false
     },
     UP_SSEQ: {
-        type: Sequelize.INTEGER.UNSIGNED,
+        type: DataType.INTEGER.UNSIGNED,
         primaryKey: true,
         allowNull: false
     },
     UP_CRCT: {
-        type: Sequelize.TINYINT(1)
+        type: DataType.TINYINT(1)
     },
     UP_DTM: {
-        type: Sequelize.DATE(3),
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)')
+        type: DataType.DATE(3),
+        defaultValue: DataType.literal('CURRENT_TIMESTAMP(3)')
     }
 });
