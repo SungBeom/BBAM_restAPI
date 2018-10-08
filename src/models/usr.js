4b05808aefd478d4d79f797705d5db29adcfc91a
SungBeom <psb_94@naver.com>
2018-10-08 15:26:18 +0900

    DB connection_USR

--- src/models/usr.js
+++ src/models/usr.js
@@ -1,35 +1,34 @@
-module.exports = (sequelize, DataType) => sequelize.define('USR',
-  {
-    USR_ID: {
-      type: DataType.STRING(10),
-      primaryKey: true,
-      allowNull: false,
-    },
-    USR_PW: {
-      type: DataType.STRING(20),
-      allowNull: false,
-    },
-    USR_NM: {
-      type: DataType.STRING(20),
-      allowNull: false,
-    },
-    USR_ADDR_ST: {
-      type: DataType.STRING(10),
-    },
-    USR_ADDR_CT: {
-      type: DataType.STRING(20),
-    },
-    USR_ADDR_STRT: {
-      type: DataType.STRING(20),
-    },
-    USR_SCHL: {
-      type: DataType.STRING(20),
-    },
-    USR_GRD: {
-      type: DataType.TINYINT,
-    },
-    USR_JNDT: {
-      type: DataType.DATE,
-      defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-    },
-  });
+module.exports = (sequelize, DataType) => sequelize.define('USR', {
+  USR_ID: {
+    type: Sequelize.STRING(10),
+    primaryKey: true,
+    allowNull: false
+  },
+  USR_PW: {
+    type: Sequelize.STRING(20),
+    allowNull: false
+  },
+  USR_NM: {
+    type: Sequelize.STRING(20),
+    allowNull: false
+  },
+  USR_ADDR_ST: {
+    type: Sequelize.STRING(10)
+  },
+  USR_ADDR_CT: {
+    type: Sequelize.STRING(20)
+  },
+  USR_ADDR_STRT: {
+    type: Sequelize.STRING(20)
+  },
+  USR_SCHL: {
+    type: Sequelize.STRING(20)
+  },
+  USR_GRD: {
+    type: Sequelize.TINYINT(1).UNSIGNED
+  },
+  USR_JNDT: {
+    type: Sequelize.DATE(3),
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)')
+  }
+});
